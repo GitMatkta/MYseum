ff8cefe88b78e0d7d946ac8c732bdf99a2ac85a1
TzeHuoGucciHo <tho22@student.aau.dk>
2023-10-25 11:45:15 +0200

    BGR Histogram testing i Gucci's Hjørne

--- Gucci's Hjørne.py
+++ Gucci's Hjørne.py
@@ -1,3 +1,19 @@
 import cv2
 import os
 
+input_image = cv2.imread(r"C:\Users\Tze Huo Gucci Ho\Desktop\Git Projects\MYseum\Malerier\Girl_with_a_Pearl_Earring.jpg")
+
+# Extract BGR histogram
+bgr_planes = cv2.split(input_image)
+histSize = 256
+histRange = (0, 256) # the upper boundary is exclusive
+accumulate = False
+b_hist = cv2.calcHist(bgr_planes, [0], None, [histSize], histRange, accumulate=accumulate)
+g_hist = cv2.calcHist(bgr_planes, [1], None, [histSize], histRange, accumulate=accumulate)
+r_hist = cv2.calcHist(bgr_planes, [2], None, [histSize], histRange, accumulate=accumulate)
+
+
+
+cv2.imshow("Original", input_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
