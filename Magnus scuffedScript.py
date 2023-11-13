571ff798620d2edba7f33b1af2a5a2b2d3a09af8
Magnus <Zy15zz@Student.aau.dk>
2023-11-13 13:23:58 +0100

    Histogram over 9000%

--- Magnus scuffedScript.py
+++ Magnus scuffedScript.py
@@ -1,45 +1,48 @@
 import cv2 as cv
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
-# Load the original color image
-image = cv.imread(r'100 Billeder cirka\Mona Lisa 7.jpg')
-#image = cv.imread(r'Malerier\Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
-# Convert the color image to the HSV color space
-hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+def calculate_channel_percentage(image):
+    total_pixels = image.size  # Total number of pixels in the image
 
-# Define the lower and upper bounds for the color you are interested in (in HSV format)
-lower_color = np.array([20, 20, 20])  # Set your lower HSV thresholds
-upper_color = np.array([255, 255, 255])  # Set your upper HSV thresholds
+    # Calculate the percentage of each color channel
+    blue_percentage = (np.sum(image[:, :, 0]) / total_pixels) * 100
+    green_percentage = (np.sum(image[:, :, 1]) / total_pixels) * 100
+    red_percentage = (np.sum(image[:, :, 2]) / total_pixels) * 100
 
-# Threshold the image to create a binary mask based on the color
-color_mask = cv.inRange(hsv, lower_color, upper_color)
+    return blue_percentage, green_percentage, red_percentage
 
-# Find contours in the binary mask
-contours, _ = cv.findContours(color_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+def plot_histogram(image, title):
+    color = ('b', 'g', 'r')
+    total_pixels = image.size  # Total number of pixels in the image
 
-# Define minimum contour area (10% of the total image area)
-min_contour_area = 0.1 * 4000 * 1800
+    for i, col in enumerate(color):
+        # Calculate histogram
+        histr = cv.calcHist([image[:, :, i]], [0], None, [256], [0, 256])
 
-# Print the number of contours found
-print(f'Number of contours found: {len(contours)}')
+        # Normalize histogram values to be percentages
+        histr_percentage = (histr / total_pixels) * 100
 
-# Find the contour with the largest area
-largest_contour = max(contours, key=cv.contourArea)
+        # Plot the histogram
+        plt.plot(histr_percentage, color=col)
+        plt.xlim([0, 256])
 
-# Draw the largest contour in blue on the original image
-cv.drawContours(image, [largest_contour], 0, (255, 0, 0), 2)
+    plt.title(title)
+    plt.show()
 
-# Extract the largest contour from the original color image
-x, y, w, h = cv.boundingRect(largest_contour)
-cropped_object = image[y:y+h, x:x+w]
+# Folder containing comparison images
+comparison_folder = r'malerier'
 
-# Calculate the average HSV values of the object
-average_hsv = np.mean(hsv[y:y+h, x:x+w], axis=(0, 1))
+# Iterate through all images in the comparison folder
+for filename in os.listdir(comparison_folder):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):
+        # Load the comparison image
+        comparison_image = cv.imread(os.path.join(comparison_folder, filename))
 
-# Print the average HSV values
-print(f'Average HSV: H={average_hsv[0]}, S={average_hsv[1]}, V={average_hsv[2]}')
+        # Calculate and print percentage values for each channel
+        blue_percent, green_percent, red_percent = calculate_channel_percentage(comparison_image)
+        print(f'Image: {filename}, Blue: {blue_percent:.2f}%, Green: {green_percent:.2f}%, Red: {red_percent:.2f}%')
 
-# Display the cropped object
-cv.imshow('Cropped Object', cropped_object)
-cv.waitKey(0)
-cv.destroyAllWindows()
+        # Display histograms for each color channel
+        plot_histogram(comparison_image, f'Histograms ({filename})')
