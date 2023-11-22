e3876609e93fe2d90bb78bdc6cd9cc35502c423c
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2023-11-22 14:07:20 +0100

    Python client works

--- newPythonClient.py
+++ newPythonClient.py
@@ -1,17 +1,39 @@
 import socket
+import struct
+import time
 
-# Define the server (Unity/C#) address and port
-server_address = ('127.0.0.1', 5555)  # Change to Unity's server IP and port
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 5555))  # Connect to the Unity server's IP and port
 
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Connected to Unity server")
 
-# Connect to the server
-client_socket.connect(server_address)
+    last_sent_value = -1  # Initialize with a value that is different from the expected range
 
-# Send a number
-number_to_send = 42  # Change to the number you want to send
-client_socket.sendall(str(number_to_send).encode())
+    try:
+        while True:
+            # Get a new custom value from user input
+            custom_value = int(input("Enter a new value (integer): "))
 
-# Close the connection
-client_socket.close()
+            # Check if the value is different from the last one
+            if custom_value != last_sent_value:
+                # Send a custom message to the server in the form of an integer
+                client.send(struct.pack('!I', custom_value))  # Convert integer to bytes
+
+                # Receive the response from the server
+                #response_message = client.recv(4)  # Assuming messages are 4 bytes (int size)
+                #response_data = struct.unpack('!I', response_message)[0]  # Convert bytes to integer
+                #print(f"Received response from Unity: {response_data}")
+
+                # Update the last sent value
+                last_sent_value = custom_value
+
+            # Wait for a moment before checking/sending the next message
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Connection closed by user.")
+    finally:
+        client.close()
+
+if __name__ == '__main__':
+    start_client()
